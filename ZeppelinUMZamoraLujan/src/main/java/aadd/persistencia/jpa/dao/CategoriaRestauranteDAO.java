f1579be4d4f4f2c3f05518dc21781cfcd0467fc5
Raúl Zamora <raulzamoracanovas@gmail.com>
2022-12-05 21:15:27 +0100

    Estaba mal el nombre del get del Dao
    Ponía getIncidenciaDAO

--- ZeppelinUMZamoraLujan/src/main/java/aadd/persistencia/jpa/dao/CategoriaRestauranteDAO.java
+++ ZeppelinUMZamoraLujan/src/main/java/aadd/persistencia/jpa/dao/CategoriaRestauranteDAO.java
@@ -1,18 +1,7 @@
 package aadd.persistencia.jpa.dao;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.Query;
-
-import aadd.persistencia.dto.IncidenciaDTO;
-import aadd.persistencia.dto.UsuarioDTO;
-import aadd.persistencia.jpa.EntityManagerHelper;
 import aadd.persistencia.jpa.bean.CategoriaRestaurante;
-import aadd.persistencia.jpa.bean.Incidencia;
-import aadd.persistencia.jpa.bean.TipoUsuario;
-import aadd.persistencia.jpa.bean.Usuario;
+
 
 public class CategoriaRestauranteDAO extends ExtensionDAO<CategoriaRestaurante> {
 
@@ -22,12 +11,13 @@ public class CategoriaRestauranteDAO extends ExtensionDAO<CategoriaRestaurante> 
 
     private static CategoriaRestauranteDAO categoriaRestauranteDAO;
 
-    public static CategoriaRestauranteDAO getIncidenciaDAO() {
+    public static CategoriaRestauranteDAO getCategoriaRestauranteDAO() {
         if (categoriaRestauranteDAO == null)
             categoriaRestauranteDAO = new CategoriaRestauranteDAO(CategoriaRestaurante.class);
         return categoriaRestauranteDAO;
     }
-    
+
+
 
    
 }
